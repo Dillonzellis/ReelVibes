36697f1fdb4d180ff38a9a3d2de3033e38c21834
dillonzellis <dillonzellis@gmail.com>
2023-09-06 14:28:56 -0400

    basic dashboard styling

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,17 @@ export default function Home() {
   return (
     <main className="bg-black">
       <div className="container">
-        <h1 className="text-white text-2xl">Movie Tracker</h1>
-        <div>
-          <UserButton afterSignOutUrl="/" />
+        <div className="flex justify-between pt-8 border-b border-white pb-4">
+          <h1 className="text-white text-5xl font-bold italic">ReelVibes</h1>
+          <div>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+        </div>
+        <div className="text-white pt-12">
+          <div>Ask for A List of Movies</div>
+          <div>give rating to each Movie</div>
+          <div>Submit Button</div>
+          <div>Give recommendation based on movies and rating</div>
         </div>
       </div>
     </main>
